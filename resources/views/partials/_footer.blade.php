069f07d496a48cbd47ceb83c32099cdd0dafc61b
Toghrul Nasirli <toghrulnasirli111@gmail.com>
2021-09-12 16:27:07 +0400

    footer done!

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,116 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 row px-md-0 pb-3">
+                <div class="col-12 col-md-5 text-center text-md-left mb-5">
+                    <p class="mb-4">Our mission is to provide a free, world-class education to anyone, anywhere.</p>
+                    <p>Khan Academy is a 501(c)(3) nonprofit organization. Donate or volunteer today!</p>
+                </div>
+                <div class="col-6 col-md-2">
+                    <ul class="mb-4">
+                        <h3 class="text-white">About</h3>
+                        <li><a href="#">News</a></a></li>
+                        <li><a href="#">Impact</a></a></li>
+                        <li><a href="#">Our team</a></li>
+                        <li><a href="#">Our interns</a></li>
+                        <li><a href="#">Our content specialists</a></li>
+                        <li><a href="#">Our leadership</a></li>
+                        <li><a href="#">Our supporters</a></li>
+                        <li><a href="#">Our contributers</a></li>
+                        <li><a href="#">Our finances</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Internships</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md-2">
+                    <ul class="mb-4">
+                        <h3>Contact</h3>
+                        <li><a href="#">Help center</a></li>
+                        <li><a href="#">Support community</a></li>
+                        <li><a href="#">Share your story</a></li>
+                        <li><a href="#">Press</a></li>
+                    </ul>
+                    <ul class="mb-4">
+                        <h3>Download our apps</h3>
+                        <li><a href="#">iOS app</a></li>
+                        <li><a href="#">Android app</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md-2">
+                    <ul class="mb-4">
+                        <h3>Careers</h3>
+                        <li><a href="#">Math: Pre-K - 8th grade</a></li>
+                        <li><a href="#">Math: Get ready courses</a></li>
+                        <li><a href="#">Math: high school & college</a></li>
+                        <li><a href="#">Test prep</a></li>
+                        <li><a href="#">Science</a></li>
+                        <li><a href="#">Computing</a></li>
+                        <li><a href="#">Arts & humanities</a></li>
+                        <li><a href="#">Economics</a></li>
+                        <li><a href="#">Reading & language arts</a></li>
+                        <li><a href="#">Life skills</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-12 text-center text-md-left px-0">
+                <div class="mb-1">
+                    <span class="mr-2">Language</span>
+                    <span class="text-white">
+                        English
+                        <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" class="_siqxmg">
+                            <path fill="#FFFFFF" d="M8 8.586l3.293-3.293a1 1 0 0 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4a1 1 0 0 1 1.414-1.414L8 8.586z"></path>
+                        </svg>
+                    </span>
+                </div>
+                <div class="mb-3">
+                    <span class="mr-3">Country</span>
+                    <a href="#">
+                        <img src="{{ asset('frontend/img/us-flag.png') }}" width="13" height="12">
+                        U.S.
+                    </a>
+                    <a href="#">
+                        <img src="{{ asset('frontend/img/india-flag.png') }}" width="13" height="12">
+                        India
+                    </a>
+                    <a href="#">
+                        <img src="{{ asset('frontend/img/mexico-flag.png') }}" width="13" height="12">
+                        Mexico
+                    </a>
+                    <a href="#">
+                        <img src="{{ asset('frontend/img/brazil-flag.png') }}" width="13" height="12">
+                        Brazil
+                    </a>
+                </div>
+            </div>
+            <div class="last-div col-12 text-center px-0 py-3">
+                <div class="row justify-content-center justify-content-md-between">
+                    <div class="px-3">
+                        <span>© 2021 Khan Academy</span>
+                        <div class="d-md-inline pb-1">
+                            <a href="#" class="px-2">Terms of use</a>
+                        </div>
+                        <div class="d-md-inline pb-1">
+                            <a href="#" class="px-2">Privacy Policy</a>
+                        </div>
+                        <div class="d-md-inline pb-3">
+                            <a href="#" class="px-2">Cookie Notice</a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-auto px-3">
+                        <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true" focusable="false" width="24" height="24" viewBox="0 0 15 15">
+                            <path fill="#FFFFFF" d="M14.172 0H.827A.827.827 0 0 0 0 .828v13.344c0 .458.37.828.828.828h7.184V9.19H6.057V6.925h1.955v-1.67c0-1.938 1.183-2.992 2.913-2.992.827 0 1.54.062 1.747.09v2.025h-1.197c-.94 0-1.123.447-1.123 1.102v1.445h2.243L12.3 9.19h-1.95V15h3.822c.458 0 .828-.37.828-.828V.827A.827.827 0 0 0 14.172 0z"></path>
+                        </svg>
+                        <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true" focusable="false" width="24" height="24" viewBox="0 0 20 20">
+                            <path fill="#FFFFFF" d="M18.895 0H1.105C.495 0 0 .495 0 1.105V18.9c0 .605.495 1.1 1.105 1.1H18.9c.61 0 1.105-.495 1.105-1.105V1.105A1.113 1.113 0 0 0 18.895 0zM14.97 7.455c.005.11.005.22.005.33 0 3.385-2.58 7.295-7.295 7.295a7.293 7.293 0 0 1-3.925-1.145A5.142 5.142 0 0 0 7.55 12.87a2.564 2.564 0 0 1-2.395-1.78c.155.03.315.045.48.045.235 0 .46-.03.675-.09A2.57 2.57 0 0 1 4.255 8.53v-.035c.345.19.74.305 1.16.32A2.57 2.57 0 0 1 4.62 5.39a7.28 7.28 0 0 0 5.285 2.68 2.567 2.567 0 0 1 2.5-3.15c.735 0 1.4.31 1.87.81a5.1 5.1 0 0 0 1.63-.62 2.58 2.58 0 0 1-1.13 1.42c.52-.065 1.015-.2 1.475-.405-.345.515-.78.97-1.28 1.33z"></path>
+                        </svg>
+                        <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true" focusable="false" width="24" height="24" viewBox="0 0 24 24">
+                            <path fill="#FFFFFF"
+                                d="M18.24,7.28a2.67,2.67,0,0,0-1.52-1.52,4.33,4.33,0,0,0-1.49-.27c-.84,0-1.09,0-3.23,0s-2.39,0-3.23,0a4.33,4.33,0,0,0-1.49.27A2.67,2.67,0,0,0,5.76,7.28a4.33,4.33,0,0,0-.27,1.49c0,.84,0,1.09,0,3.23s0,2.39,0,3.23a4.33,4.33,0,0,0,.27,1.49,2.67,2.67,0,0,0,1.52,1.52,4.33,4.33,0,0,0,1.49.27c.84,0,1.09,0,3.23,0s2.39,0,3.23,0a4.33,4.33,0,0,0,1.49-.27,2.67,2.67,0,0,0,1.52-1.52,4.33,4.33,0,0,0,.27-1.49c0-.84,0-1.09,0-3.23s0-2.39,0-3.23A4.33,4.33,0,0,0,18.24,7.28ZM12,16.11A4.11,4.11,0,1,1,16.11,12,4.11,4.11,0,0,1,12,16.11Zm4.27-7.42a1,1,0,1,1,1-1A1,1,0,0,1,16.27,8.69Z M12,9.33A2.67,2.67,0,1,0,14.67,12,2.68,2.68,0,0,0,12,9.33Z M22,0H2A2,2,0,0,0,0,2V22a2,2,0,0,0,2,2H22a2,2,0,0,0,2-2V2A2,2,0,0,0,22,0ZM20,15.3a5.85,5.85,0,0,1-.37,1.94,4,4,0,0,1-2.34,2.34A5.85,5.85,0,0,1,15.3,20c-.85,0-1.13.05-3.3.05S9.55,20,8.7,20a5.85,5.85,0,0,1-1.94-.37,4,4,0,0,1-2.34-2.34,5.85,5.85,0,0,1-.37-1.94c0-.85,0-1.13,0-3.3s0-2.45,0-3.3a5.85,5.85,0,0,1,.37-1.94A4,4,0,0,1,6.76,4.42,5.85,5.85,0,0,1,8.7,4.05C9.55,4,9.83,4,12,4s2.45,0,3.3,0a5.85,5.85,0,0,1,1.94.37,4,4,0,0,1,2.34,2.34A5.85,5.85,0,0,1,20,8.7c0,.85.05,1.13.05,3.3S20,14.45,20,15.3Z">
+                            </path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
